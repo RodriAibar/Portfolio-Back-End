256e217933fbcb837b7b14528d8b5a669f755702
Rodrigo Aibar <99849205+RodriAibar@users.noreply.github.com>
2022-08-24 08:37:12 -0300

    mi primer commit

---
+++ Rodrigo/src/test/java/com/portfolio/Rodrigo/RodrigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Rodrigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodrigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
